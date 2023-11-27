363d1f83872f9a06ce17e9ad680e5257dc99a80d
Jarl Evanson <evanson.jarl@gmail.com>
2023-11-27 13:50:28 -0600

    Implemented file loading and validation

--- bootloader/src/filesystem.rs
+++ bootloader/src/filesystem.rs
@@ -1,11 +1,23 @@
+//! Basic utilities related to loading and validating a file.
+
+use core::{mem::MaybeUninit, fmt::Display};
+
+use digest::sha512::{bytes::Sha512, Digest};
 use uefi::{
     boot::acquire_boot_handle,
+    data_types::Align,
     proto::{
         loaded_image::LoadedImage,
-        media::{file::Directory, fs::SimpleFileSystem},
+        media::{
+            file::{Directory, File, FileAttribute, FileInfo, FileMode},
+            fs::SimpleFileSystem,
+        },
     },
     table::boot::{OpenProtocolAttributes, OpenProtocolParams},
+    CStr16, Status,
 };
+
+use crate::vec::Vec;
 
 /// Acquire the root directory of the boot partition.
 ///
@@ -62,7 +74,179 @@ pub fn acquire_boot_partition_root_directory() -> Result<Directory, AcquireRootE
     Ok(root_directory)
 }
 
+/// Various errors that occur when running [`acquire_boot_partition_root_directory`].
 pub enum AcquireRootError {
+    /// Bootloader was not loaded from a device or bootloader was loaded from a device
+    /// that does not support the [`SimpleFileSystem`] protocol.
     InvalidBootMethod,
+    /// Opening the volume from which the bootloader image was loaded failed.
     InvalidVolume,
+}
+
+/// Loads a file from the specified [`Directory`] and validates that it matches the digest.
+///
+/// # Errors
+/// - [`NotFound`][nfo]
+///     - File was not found
+/// - [`MediaError`][me]
+///     - The underlying media changed
+///     - There exists no underlying media
+///     - A device errror occured
+/// - [`VolumeCorrupted`][vc]
+///     - The volume was corrupted
+/// - [`AccessDenied`][ad]
+///     - Access to the file was denied
+/// - [`OutOfResources`][oor]
+///     - Not enough resources were available to open the file
+///     - A memory allocation error occured
+/// - [`NotFile`][nfi]
+///     - The file was found, but was not a file
+/// - [`InvalidDigest`][id]
+///     - The cryptographic hash of the loaded file did not match the expected hash
+///
+/// [nfo]: LoadFileError::NotFound
+/// [me]: LoadFileError::MediaError
+/// [vc]: LoadFileError::VolumeCorrupted
+/// [ad]: LoadFileError::AccessDenied
+/// [oor]: LoadFileError::OutOfResources
+/// [nfi]: LoadFileError::NotFile
+/// [id]: LoadFileError::InvalidDigest
+#[expect(
+    clippy::missing_panics_doc,
+    reason = "only panic is the assertion that the alignment of` FileInfo` is at most 8"
+)]
+pub fn load_file(
+    directory: &mut Directory,
+    path: &CStr16,
+    valid_digest: Digest,
+) -> Result<Vec<u8>, LoadFileError> {
+    log::trace!(target: "filesystem", "attempting to load {}", path);
+
+    let file = match directory.open(path, FileMode::Read, FileAttribute::empty()) {
+        Ok(file) => file,
+        Err(err) => match err.status() {
+            Status::NOT_FOUND => return Err(LoadFileError::NotFound),
+            Status::MEDIA_CHANGED | Status::NO_MEDIA | Status::DEVICE_ERROR => {
+                return Err(LoadFileError::MediaError)
+            }
+            Status::VOLUME_CORRUPTED => return Err(LoadFileError::VolumeCorrupted),
+            Status::ACCESS_DENIED => return Err(LoadFileError::AccessDenied),
+            Status::OUT_OF_RESOURCES => return Err(LoadFileError::OutOfResources),
+            general => unreachable!("unexpected error: {:?}", general),
+        },
+    };
+
+    log::trace!(target: "filesystem", "acquired file handle");
+
+    let mut file = file.into_regular_file().ok_or(LoadFileError::NotFile)?;
+
+    log::trace!(target: "filesystem", "opened file");
+
+    assert!(
+        <FileInfo as Align>::alignment() <= 8,
+        "allocate_pool is only guaranteed to allocate 8-byte aligned pools"
+    );
+
+    let info_size = match file.get_info::<FileInfo>(&mut []) {
+        Ok(_) => unreachable!(),
+        Err(err) => match err.split() {
+            (Status::UNSUPPORTED, _) => return Err(LoadFileError::NotFile),
+            (Status::NO_MEDIA | Status::DEVICE_ERROR, _) => return Err(LoadFileError::MediaError),
+            (Status::VOLUME_CORRUPTED, _) => return Err(LoadFileError::VolumeCorrupted),
+            (Status::BUFFER_TOO_SMALL, size) => {
+                size.expect("expected buffer size must be available")
+            }
+            general => unreachable!("unexpected error: {:?}", general),
+        },
+    };
+
+    let mut vec = Vec::<u8>::with_capacity(info_size).map_err(|_| LoadFileError::OutOfResources)?;
+
+    vec.spare_capacity_mut().fill(MaybeUninit::new(0));
+
+    // SAFETY:
+    // `vec.capacity()` is less than or equal to `capacity`.
+    // Uninitialized memory has been filled with zeros.
+    unsafe {
+        vec.set_len(vec.capacity());
+    }
+
+    let info = match file.get_info::<FileInfo>(vec.as_slice_mut()) {
+        Ok(info) => info,
+        Err(err) => match err.split() {
+            (Status::UNSUPPORTED, _) => return Err(LoadFileError::NotFile),
+            (Status::NO_MEDIA | Status::DEVICE_ERROR, _) => return Err(LoadFileError::MediaError),
+            (Status::VOLUME_CORRUPTED, _) => return Err(LoadFileError::VolumeCorrupted),
+            general => unreachable!("unexpected error: {:?}", general),
+        },
+    };
+
+    let required_bytes =
+        TryInto::<usize>::try_into(info.file_size()).map_err(|_| LoadFileError::OutOfResources)?;
+
+    vec.try_reserve(required_bytes.saturating_sub(vec.len()))
+        .map_err(|_| LoadFileError::OutOfResources)?;
+
+    vec.spare_capacity_mut().fill(MaybeUninit::new(0));
+
+    // SAFETY:
+    // `vec.capacity()` is less than or equal to `capacity`.
+    // Uninitialized memory has been filled with zeros.
+    unsafe {
+        vec.set_len(vec.capacity());
+    }
+
+    match file.read(vec.as_slice_mut()) {
+        Ok(bytes) => {
+            log::trace!(target: "filesystem", "read {bytes} bytes out of {required_bytes} total bytes");
+        }
+        Err(_) => return Err(LoadFileError::MediaError),
+    }
+
+    let mut sha512 = Sha512::new();
+
+    sha512.update(vec.as_slice_mut()).unwrap();
+
+    let digest = sha512.finalize();
+
+    if digest != valid_digest {
+        return Err(LoadFileError::InvalidDigest);
+    }
+
+    log::trace!(target: "filesystem", "validated file digest");
+
+    Ok(vec)
+}
+
+/// Various errors occurring when attempting to load a file.
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub enum LoadFileError {
+    /// The file was not found.
+    NotFound,
+    /// An error occurred when locating or reading from the media.
+    MediaError,
+    /// There are not enough resources to load the file.
+    OutOfResources,
+    /// The requested file is not a file.
+    NotFile,
+    /// Access to the file or media was denied.
+    AccessDenied,
+    /// The volume's structure was corrupted.
+    VolumeCorrupted,
+    /// The digest of the file did not match the expected digest.
+    InvalidDigest,
+}
+
+impl Display for LoadFileError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            LoadFileError::NotFound => f.write_str("the requested file was not found"),
+            LoadFileError::MediaError => f.write_str("a media error ocurred"),
+            LoadFileError::OutOfResources => f.write_str("out of resources"),
+            LoadFileError::NotFile => f.write_str("requested item was not a file"),
+            LoadFileError::AccessDenied => f.write_str("access to the file was not allowed"),
+            LoadFileError::VolumeCorrupted => f.write_str("the voluem was corrupted"),
+            LoadFileError::InvalidDigest => f.write_str("the digest of the file was unexpected: verify its authenticity"),
+        }
+    }
 }
--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -4,15 +4,35 @@
 #![no_main]
 #![feature(lint_reasons, maybe_uninit_slice, strict_provenance)]
 
-use filesystem::{acquire_boot_partition_root_directory, AcquireRootError};
+use digest::sha512::Digest;
+use filesystem::{
+    acquire_boot_partition_root_directory, load_file, AcquireRootError, LoadFileError,
+};
 use uefi::{
     table::{Boot, SystemTable},
-    Handle, Status,
+    CStr16, Handle, Status,
 };
 
 pub mod filesystem;
 pub mod logging;
 pub mod vec;
+
+/// The path to the configuration file.
+const CONFIG_PATH: &CStr16 = uefi::cstr16!("zeta\\config.toml");
+
+/// The current digest of the test configuration file.
+#[export_name = "digest"]
+#[link_section = ".config"]
+static CONFIG_DIGEST: Digest = Digest::from_u64s([
+    0x0340_f948_f096_c1cb,
+    0xd1b7_3efd_a6f5_4a49,
+    0xc28b_8bd9_397a_ba28,
+    0x5abf_8552_293e_dde6,
+    0x4fc3_763a_0db9_cad7,
+    0x53bd_1e19_03a7_6d65,
+    0x18de_de79_36ae_3896,
+    0xe699_4aad_8db6_4eb3,
+]);
 
 #[uefi_macros::entry]
 fn main(image_handle: Handle, system_table: SystemTable<Boot>) -> Status {
@@ -21,7 +41,7 @@ fn main(image_handle: Handle, system_table: SystemTable<Boot>) -> Status {
         Err(_err) => return Status::ABORTED,
     }
 
-    let root_dir = match acquire_boot_partition_root_directory() {
+    let mut root_dir = match acquire_boot_partition_root_directory() {
         Ok(root_dir) => {
             log::trace!("acquired root directory of boot partition");
             root_dir
@@ -39,9 +59,47 @@ fn main(image_handle: Handle, system_table: SystemTable<Boot>) -> Status {
         }
     };
 
-    Status::SUCCESS
+    log::info!(target: "config", "loading configuration file");
+    let result = match load_file(&mut root_dir, CONFIG_PATH, CONFIG_DIGEST) {
+        Ok(bytes) => bytes,
+        Err(LoadFileError::AccessDenied) => {
+            log::error!("access to file was not allowed");
+            return Status::ACCESS_DENIED;
+        }
+        Err(LoadFileError::InvalidDigest) => {
+            log::error!("the digest of the configuration file was unexpected: verify authenticity and then embed its hash");
+            return Status::SECURITY_VIOLATION;
+        }
+        Err(LoadFileError::MediaError) => {
+            log::error!("a media error occurred");
+            return Status::ABORTED;
+        }
+        Err(LoadFileError::NotFile) => {
+            log::error!("item at \"{CONFIG_PATH}\" was not a file");
+            return Status::INVALID_PARAMETER;
+        }
+        Err(LoadFileError::NotFound) => {
+            log::error!("a config file must exist");
+            return Status::NOT_FOUND;
+        }
+        Err(LoadFileError::OutOfResources) => {
+            log::error!("out of resources");
+            return Status::OUT_OF_RESOURCES;
+        }
+        Err(LoadFileError::VolumeCorrupted) => {
+            log::error!("the volume was corrupted");
+            return Status::VOLUME_CORRUPTED;
+        }
+    };
+
+    loop {
+        core::hint::spin_loop();
+    }
 }
 
+/// The panic handler for the bootloader.
+///
+/// Logs the panic message and then spins.
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {

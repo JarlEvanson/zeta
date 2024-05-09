b154eb7915e98af12176ecab44fb76c839fc269f
Jarl Evanson <evanson.jarl@gmail.com>
2024-05-09 17:40:44 -0400

    Updated `build.rs` and `main.rs` to enable testing

--- kernel/build.rs
+++ kernel/build.rs
@@ -3,9 +3,12 @@
 fn main() {
     println!("cargo::rerun-if-changed=kernel/build.rs");
 
-    #[cfg(target_arch = "x86_64")]
-    {
-        println!("cargo::rerun-if-changed=kernel/linker_scripts/x86_64.ld");
-        println!("cargo::rustc-link-arg=-Tkernel/linker_scripts/x86_64.ld");
+    // When testing, we shouldn't use the custom linker scripts.
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "none" {
+        #[cfg(target_arch = "x86_64")]
+        {
+            println!("cargo::rerun-if-changed=kernel/linker_scripts/x86_64.ld");
+            println!("cargo::rustc-link-arg=-Tkernel/linker_scripts/x86_64.ld");
+        }
     }
 }
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,15 +1,18 @@
 //! Kernel for the zeta project.
 
-#![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
 
 pub mod arch;
 pub mod cells;
 pub mod polyfill;
 pub mod utils;
 
+#[cfg(test)]
+fn main() {}
+
 /// Function that handles panics.
-#[panic_handler]
+#[cfg_attr(not(test), panic_handler)]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {
         core::hint::spin_loop();

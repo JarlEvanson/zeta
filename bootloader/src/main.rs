ffbfe76706de98c875095b83c7cf86d91cb9103d
Jarl Evanson <evanson.jarl@gmail.com>
2024-05-21 00:11:07 -0400

    Implement basic setup for stdout and stderr for UEFI at entry

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -4,7 +4,10 @@
 #![no_main]
 
 use crate::uefi::tables::system::{Boot, SystemTable};
-use ::uefi::datatypes::Status;
+use ::uefi::{
+    datatypes::Status,
+    protocols::console::text::{BackgroundColor, ForegroundColor},
+};
 
 mod uefi;
 
@@ -12,9 +15,66 @@ entry_point!(entry_point);
 
 /// The main logic for the bootloader.
 fn entry_point(mut system_table: SystemTable<Boot>) -> Status {
+    setup_outputs(&mut system_table);
+
     system_table.boot_services().stall(10_000_000);
 
     Status::SUCCESS
+}
+
+/// Sets up console out and console err for the executable.
+fn setup_outputs(system_table: &mut SystemTable<Boot>) {
+    let mut console_out = system_table.console_out();
+
+    let max_mode = console_out.info().max_mode as usize;
+
+    let mut best_mode = 0;
+
+    let mut best_rows = 0;
+    let mut best_columns = 0;
+
+    for mode in 0..max_mode {
+        let Ok(dimensions) = console_out.query_mode(mode) else {
+            continue;
+        };
+
+        if dimensions.rows > best_rows || dimensions.columns > best_columns {
+            best_mode = mode;
+
+            best_rows = dimensions.rows;
+            best_columns = dimensions.columns;
+        }
+    }
+
+    let _ = console_out.set_mode(best_mode);
+    let _ = console_out.enable_cursor(false);
+    let _ = console_out.set_attribute(BackgroundColor::Black, ForegroundColor::Green);
+
+    let mut console_err = system_table.console_err();
+
+    let max_mode = console_err.info().max_mode as usize;
+
+    let mut best_mode = 0;
+
+    let mut best_rows = 0;
+    let mut best_columns = 0;
+
+    for mode in 0..max_mode {
+        let Ok(dimensions) = console_err.query_mode(mode) else {
+            continue;
+        };
+
+        if dimensions.rows > best_rows || dimensions.columns > best_columns {
+            best_mode = mode;
+
+            best_rows = dimensions.rows;
+            best_columns = dimensions.columns;
+        }
+    }
+
+    let _ = console_err.set_mode(best_mode);
+    let _ = console_err.enable_cursor(false);
+    let _ = console_err.set_attribute(BackgroundColor::Black, ForegroundColor::Red);
 }
 
 /// Handles panics occurring while booting the system.

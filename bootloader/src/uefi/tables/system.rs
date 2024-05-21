90d6f136eb9282851797436c2645525adb345ded
Jarl Evanson <evanson.jarl@gmail.com>
2024-05-21 00:10:09 -0400

    Implement alternative wrapper for `SystemTable` provided protocols due to constraints about the implementation

--- bootloader/src/uefi/tables/system.rs
+++ bootloader/src/uefi/tables/system.rs
@@ -4,7 +4,10 @@ use core::{marker::PhantomData, ptr::NonNull};
 
 use uefi::tables::{boot_services::RawBootServicesTable, system::RawSystemTable};
 
-use crate::uefi::tables::boot::BootServicesTable;
+use crate::uefi::{
+    protocols::{console::text::SimpleTextOutput, Protocol},
+    tables::boot::BootServicesTable,
+};
 
 /// A UEFI System Table.
 pub struct SystemTable<T> {
@@ -51,6 +54,60 @@ impl SystemTable<Boot> {
             ptr: boot_services_ptr,
             lifetime: PhantomData,
         }
+    }
+
+    /// Returns the associated [`SimpleTextOutput`] used as console out.
+    pub fn console_out(&mut self) -> SystemTableProtocol<SimpleTextOutput> {
+        // SAFETY:
+        // `self.ptr` points to a valid [`RawSystemTable`].
+        let console_out_ptr = unsafe { (*self.ptr.as_ptr()).console_out };
+        let console_out =
+            // SAFETY:
+            // `self.ptr` points to a valid [`RawSystemTable`].
+            unsafe { SimpleTextOutput::from_ffi_ptr(console_out_ptr.cast::<core::ffi::c_void>()) };
+
+        SystemTableProtocol {
+            protocol: console_out,
+            lifetime: PhantomData,
+        }
+    }
+
+    /// Returns the associated [`SimpleTextOutput`] used as console out.
+    pub fn console_err(&mut self) -> SystemTableProtocol<SimpleTextOutput> {
+        // SAFETY:
+        // `self.ptr` points to a valid [`RawSystemTable`].
+        let console_err_ptr = unsafe { (*self.ptr.as_ptr()).console_err };
+        let console_err =
+            // SAFETY:
+            // `self.ptr` points to a valid [`RawSystemTable`].
+            unsafe { SimpleTextOutput::from_ffi_ptr(console_err_ptr.cast::<core::ffi::c_void>()) };
+
+        SystemTableProtocol {
+            protocol: console_err,
+            lifetime: PhantomData,
+        }
+    }
+}
+
+/// [`Protocol`] interfaces offered by a [`SystemTable<Boot>`].
+pub struct SystemTableProtocol<'lifetime, T: Protocol> {
+    /// The [`Protocol`] struct contains.
+    protocol: T,
+    /// Maintain's the proper lifetime from the [`SystemTable<Boot>`].
+    lifetime: PhantomData<&'lifetime T>,
+}
+
+impl<T: Protocol> core::ops::Deref for SystemTableProtocol<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.protocol
+    }
+}
+
+impl<T: Protocol> core::ops::DerefMut for SystemTableProtocol<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.protocol
     }
 }
 
